ef81b232294dd37161307e3cb00f3c4a390fdbe3
machuang <358017672@qq.com>
2019-06-03 20:52:05 +0800

    first commit

---
+++ src/main/java/com/example/stimulate/StimulateApplication.java
@@ -0,0 +1,13 @@
+package com.example.stimulate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StimulateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StimulateApplication.class, args);
+	}
+
+}
