bcbd464d52b0aaa206472670e821a4ce16a4d0a7
Administrator <358017672@qq.com>
2020-03-25 16:19:21 +0800

    111

--- src/main/java/com/example/stimulate/controller/IndexConrtoller.java
+++ src/main/java/com/example/stimulate/controller/IndexConrtoller.java
@@ -4,16 +4,19 @@ import com.example.stimulate.service.PlanService;
 import com.example.stimulate.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RequestMapping("index")
+@RestController
 public class IndexConrtoller {
 
     @Autowired
     private PlanService planService;
     @RequestMapping("test")
     public R test(){
+        System.out.println("输出方法了");
        Map map =  planService.selectOne();
         return R.succse(map);
     }
